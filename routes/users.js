2b8182943d87e7d563bf766353bd1188c6c24701
Cheri <avyishi@gmail.com>
2015-05-21 15:58:19 -0700

    Changes to users and global

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET userlist */
 router.get('/userlist', function(req, res) {
-  var db = req.db;
-  db.collection('userlist').find().toArray(function(er, items) {
-    res.json(items);
-  });
-}); 
+    var db = req.db;
+    db.collection('userlist').find().toArray(function (err, items) {
+        res.json(items);
+    });
+});
 
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    db.collection('userlist').insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
 
 module.exports = router;
